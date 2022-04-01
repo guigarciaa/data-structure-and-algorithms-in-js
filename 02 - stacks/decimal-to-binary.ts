6697df29f643877e4c4a3c9522ba9dec2f1e3cdf
guigarciaa <guilherme_designer@hotmail.com>
2022-04-01 19:28:02 -0300

    add: decimal to binary

---
+++ 02 - stacks/decimal-to-binary.ts
@@ -0,0 +1,19 @@
+import Stack, { IStack } from '.';
+
+const decimalToBinary = (decNumber: number) => {
+  let remStack: IStack<number> = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = '';
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+};
